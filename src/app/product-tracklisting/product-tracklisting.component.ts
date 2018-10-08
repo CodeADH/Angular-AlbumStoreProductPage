5d4db07818b08d822ba4122386ccd213622afe56
CodeADH <36961758+CodeADH@users.noreply.github.com>
2018-10-08 21:46:57 +0530

    Update src/app/product-tracklisting/product-tracklisting.component.ts

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -8,9 +8,9 @@ import { ProductService } from '../product.service';
   styleUrls: ['./product-tracklisting.component.css']
 })
 export class ProductTracklistingComponent implements OnInit {
-
-  constructor(private _productService:ProductService) { }
   albumInfo : Album;
+  constructor(private _productService:ProductService) { }
+  
 
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(
